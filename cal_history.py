59e1b1d40064d6f96d54e4067f92880d38973a6a
luoyueyizhan <89053319+luoyueyizhan@users.noreply.github.com>
2021-08-29 01:06:05 +0800

    Update cal_history.py

--- cal_history.py
+++ cal_history.py
@@ -2,6 +2,15 @@ import tkinter as tk
 
 
 def write_history(display,ans):
+    """History write.
+
+    Write anser in the text box and save the equation in history file.
+
+    Args:
+        ans(str):The answer of the equation
+        display(Any): Some function and values about the text of the window
+        history_file(TextIOWrapper):The file of history
+    """
     display.insert(tk.END,'=')
     display.insert(tk.END,ans)
     ans=display.get(1.0,tk.END)
@@ -12,6 +21,18 @@ def write_history(display,ans):
 
 
 def read_history(root):
+    """Read history.
+
+    Read the history equations which save in the history files.
+    
+    Args:
+        root(Tk): the window
+        history_file(TextIOWrapper):the file of history.
+        strs(str):the equtions in the file.
+        window_history(Toplevel): a new window used to show the history
+        history_eqution(Text):a text box used to show the history on the window
+        i(str): a equaiton in the history file
+    """
     history_file=open("history.txt","r+")
     strs=history_file.read()
     window_history=tk.Toplevel(root)
