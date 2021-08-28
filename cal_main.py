d260896fbaa111f1d6caebfaead9f8de6ac67d8e
luoyueyizhan <89053319+luoyueyizhan@users.noreply.github.com>
2021-08-29 01:06:43 +0800

    Update cal_main.py

--- cal_main.py
+++ cal_main.py
@@ -3,6 +3,16 @@ from cal_UI_Number import Number
 from cal_UI_Menu import cal_menu
 
 def run_cal():
+    """Main procedure.
+
+    the main procedure of the calculator.
+
+    Args:
+        root(Tk):some value and function of the window.
+        display(Text):the text box of the window.
+        Number(Number):the number keyborad
+        calmenu(calmenu):the menu of the window.
+    """
     root=tk.Tk(className='calculator')
     root.geometry('330x480')
     display=tk.Text(root,width=330,height=5,undo=True)
@@ -12,4 +22,3 @@ def run_cal():
     root.mainloop()
 
 run_cal()
-# %%
